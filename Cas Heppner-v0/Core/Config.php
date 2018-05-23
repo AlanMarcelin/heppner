e3a922f2afe9e92a6dce26287694dd14ce08ffbb
AlanMarcelin <35603722+AlanMarcelin@users.noreply.github.com>
2018-05-23 15:11:22 +0200

    Add files via upload

---
+++ Cas Heppner-v0/Core/Config.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Core;
+
+class Config{
+    // For database settings
+    const BDD_HOST      = 'integ-sql-d.sio.lan';
+    const BDD_NAME      = 'heppner_d_v0';
+    const BDD_USER      = 'usergrant';
+    const BDD_PASSWORD  = 'pwgrant';
+
+    // For Application settings
+    const APPLI_NAME    = 'Heppner';
+    const APPLI_VERSION = 'v0';
+}
